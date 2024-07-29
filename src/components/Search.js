883e5d9ed8282cf48de58ea8f122ce22b0c04c57
esot0001 <esot0001@algonquinlive.com>
2024-07-29 13:02:35 -0400

    feat: displays job search results

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,31 +1,59 @@
 import {
-    getJobs,
-    API_URL,
-    searchFormEl,
-    searchInputEl,
-    numberEl
+  getJobs,
+  API_URL,
+  searchFormEl,
+  searchInputEl,
+  numberEl,
+  jobListSearchEl,
 } from "../common.js";
 
-//The instructor added a SUBMIT event listener to the search form while I added an INPUT handler to the search input 
-const searchInputHandler = async () =>{
-    let searchTerm = searchInputEl.value;
-    console.log(searchTerm);
-    
-    try{
-        const jobs = await getJobs(`${API_URL}?search=${searchTerm}`);
+//The instructor added a SUBMIT event listener to the search form while I added an INPUT handler to the search input
+const searchInputHandler = async () => {
+  let searchTerm = searchInputEl.value;
+  console.log(searchTerm);
 
-        //destructre job items
-        const { jobItems } = jobs;
-        console.log(jobItems);
+  try {
+    const jobs = await getJobs(`${API_URL}?search=${searchTerm}`);
 
-        //count and display number of search results
-        numberEl.textContent = jobItems.length;
-    } catch(error){
-        console.log("Error: " + error)
-    }
+    //destructre job items
+    const { jobItems } = jobs;
+    console.log(jobItems);
 
-    //remove focus from input after typing search term
-    searchInputEl.blur();
-}
+    //count and display number of search results
+    numberEl.textContent = jobItems.length;
 
-searchInputEl.addEventListener('input', searchInputHandler);
+    //display job items
+    jobItems.slice(0, 7).forEach((jobItem) => {
+      const jobItemHTML = `
+            <li class="job-item">
+                <a class="job-item__link" href="${jobItem.id}">
+                    <div class="job-item__badge">${jobItem.badgeLetters}</div>
+                    <div class="job-item__middle">
+                        <h3 class="third-heading">${jobItem.title}</h3>
+                        <p class="job-item__company">${jobItem.company}</p>
+                        <div class="job-item__extras">
+                            <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i> ${jobItem.duration}</p>
+                            <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i> ${jobItem.salary}</p>
+                            <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i> ${jobItem.location}</p>
+                        </div>
+                    </div>
+                    <div class="job-item__right">
+                        <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
+                        <time class="job-item__time">${jobItem.daysAgo}</time>
+                    </div>
+                </a>
+            </li>
+        `;
+      jobListSearchEl.insertAdjacentHTML("beforeend", jobItemHTML);
+    });
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+
+  //remove focus from input after typing search term
+  searchInputEl.blur();
+
+  //display search results
+};
+
+searchInputEl.addEventListener("input", searchInputHandler);
